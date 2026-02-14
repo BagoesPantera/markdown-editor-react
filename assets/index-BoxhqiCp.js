c507d8dd707461f40b138313977f4e285d416d1b
BagoesPantera <bagoespantera01@gmail.com>
2026-02-15 07:54:35 +0800

    dist

---
+++ assets/index-BoxhqiCp.js
@@ -0,0 +1,1 @@
+import{j as r}from"./index-BXYruQWd.js";import{R as e}from"./ReactMarkdownComponent-BB1vDDEV.js";function n(){return r.jsx("div",{className:"container",children:r.jsx(e,{})})}export{n as default};
---
+++ assets/index-CAB4sPQG.js
@@ -0,0 +1,2 @@
+import{r,C as a,j as t}from"./index-BXYruQWd.js";function i(){const{code:e}=r.useContext(a);return t.jsx("div",{className:"container-fluid font-monospace bg-dark text-light min-vh-100",children:e.split(`
+`).map((s,n)=>t.jsx(t.Fragment,{children:t.jsxs("p",{className:"my-0",children:[s," "]},n)}))})}export{i as default};
